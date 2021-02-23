42b55176a402f94af133dcdb16008f424ad07150
Austin S. Hemmelgarn <austin@netdata.cloud>
2021-02-23 10:09:18 -0500

    Switch to ruamel.yaml to preserve formatting properly.

--- .github/scripts/update-agent-version.py
+++ .github/scripts/update-agent-version.py
@@ -4,14 +4,18 @@ import pathlib
 import sys
 
 import semver
-import yaml
+
+from ruamel.yaml import YAML
 
 PREFIX = pathlib.PurePath('charts', 'netdata')
 
 NEW_APP_VERSION = sys.argv[1]
 
+yaml = YAML(typ='rt')
+yaml.indent(mapping=2, sequence=4, offset=2)
+
 with open(PREFIX.joinpath('Chart.yaml'), 'r') as chart:
-    chart_data = yaml.safe_load(chart)
+    chart_data = yaml.load(chart)
 
 old_chart_version = semver.VersionInfo.parse(chart_data['version'])
 new_chart_version = old_chart_version.next_version(part='patch')
@@ -22,7 +26,7 @@ chart_data['version'] = str(new_chart_version)
 chart_data['appVersion'] = NEW_APP_VERSION
 
 with open(PREFIX.joinpath('Chart.yaml'), 'w') as chart:
-    chart.write(yaml.dump(chart_data, default_flow_style=False, sort_keys=False))
+    yaml.dump(chart_data, chart)
 
 with open(PREFIX.joinpath('README.md'), 'r+') as readme:
     data = readme.read()
